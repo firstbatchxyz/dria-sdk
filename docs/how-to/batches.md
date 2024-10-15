8a99450f741c2cc28a804bfcec26a5d7ab45d7e7
andthattoo <omer@firstbatch.xyz>
2024-10-15 11:07:08 +0300

    updated batches updated docs

--- docs/how-to/batches.md
+++ docs/how-to/batches.md
@@ -8,6 +8,7 @@ To use Batches, you need to create a `Dria` client, a `Singleton` task, and a `P
 ```python
 from dria.client import Dria
 from dria.factory import Simple
+from dria.models import Model
 from dria.batches import ParallelSingletonExecutor
 import asyncio
 
@@ -15,6 +16,7 @@ async def batch():
     dria_client = Dria()
     singleton = Simple()
     executor = ParallelSingletonExecutor(dria_client, singleton)
+    executor.set_models([Model.QWEN2_5_7B_FP16, Model.LLAMA3_2_3B, Model.LLAMA3_2_1B])
     executor.load_instructions([{ "prompt": "What is the capital of France?" }, { "prompt": "What is the capital of Germany?" }])
     return await executor.run()
 
--- dria/batches/batches.py
+++ dria/batches/batches.py
@@ -14,9 +14,13 @@ class ParallelSingletonExecutor:
         self.singleton = singleton
         self.batch_size = batch_size
         self.instructions: List[Task] = []
+        self.models = [Model.OLLAMA]
 
     def load_instructions(self, inputs: List[Dict[str, Any]]):
         [self.instructions.append(self._create_task(inp)) for inp in inputs]
+
+    def set_models(self, models: List[Model]):
+        self.models = models
 
     async def execute_workflows(self):
         all_results = []
@@ -30,7 +34,7 @@ class ParallelSingletonExecutor:
 
     def _create_task(self, data: Dict[str, Any]) -> Task:
         workflow_data = self.singleton.workflow(**data).model_dump()
-        return Task(workflow=workflow_data, models=[data.get("model", Model.OLLAMA)])
+        return Task(workflow=workflow_data, models=self.models)
 
     def _parse_results(self, results: List[Any]) -> List[Any]:
         return self.singleton.parse_result(results)
