332aa2b75e4ae5cb7c1d7ce4d1ccc21e63cb9c1f
anilaltuner <anil@firstbatch.xyz>
2024-10-20 16:57:09 +0300

    timeout to batches

--- dria/batches/batches.py
+++ dria/batches/batches.py
@@ -1,20 +1,22 @@
-from typing import List, Union, Tuple, Dict, Any
+from typing import List, Dict, Any
+
 from dria.client import Dria
-from dria.models import Task, Model
 from dria.factory.workflows.template import SingletonTemplate
+from dria.models import Task, Model
 
 MAX_CONCURRENT: int = 1000
 
 
 class ParallelSingletonExecutor:
     def __init__(
-        self, dria_client: Dria, singleton: SingletonTemplate, batch_size: int = 100
+            self, dria_client: Dria, singleton: SingletonTemplate, batch_size: int = 100
     ):
         self.dria = dria_client
         self.singleton = singleton
         self.batch_size = batch_size
         self.instructions: List[Task] = []
         self.models = [Model.OLLAMA]
+        self.timeout = 60
 
     def load_instructions(self, inputs: List[Dict[str, Any]]):
         [self.instructions.append(self._create_task(inp)) for inp in inputs]
@@ -22,12 +24,15 @@ class ParallelSingletonExecutor:
     def set_models(self, models: List[Model]):
         self.models = models
 
+    def set_timeout(self, timeout: int):
+        self.timeout = timeout
+
     async def execute_workflows(self):
         all_results = []
         for i in range(0, len(self.instructions), self.batch_size):
-            batch = self.instructions[i : i + self.batch_size]
+            batch = self.instructions[i: i + self.batch_size]
             # Execute tasks in batches, respecting the max_concurrent limit
-            results = await self.dria.execute(batch)
+            results = await self.dria.execute(batch, timeout=self.timeout)
             parsed_results = self._parse_results(results)
             all_results.extend(parsed_results)
         return all_results
