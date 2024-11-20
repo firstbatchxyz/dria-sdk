fa85efd067c7b35c04f415c75845dfae9a40ece7
andthattoo <omer@firstbatch.xyz>
2024-11-20 16:14:55 +0300

    model updates

--- examples/rag_evaluation/run.py
+++ examples/rag_evaluation/run.py
@@ -19,8 +19,8 @@ async def run_qa_pipeline(dria: Dria, file_chunks):
 
     pipeline = QAPipeline(dria, config=PipelineConfig()).build(
         simulation_description="AI developers and researchers learning Huggingface. "
-        "Some focus on fine-tuning and post-training, others on RAG systems, "
-        "retrieval problems, image models, or dataset work.",
+                               "Some focus on fine-tuning and post-training, others on RAG systems, "
+                               "retrieval problems, image models, or dataset work.",
         num_samples=1,
         persona="A HuggingFace expert that is concise and direct",
         chunks=file_chunks,
@@ -34,7 +34,7 @@ async def run_multihop_tasks(dria: Dria, file_chunks):
     executor = ParallelSingletonExecutor(dria, singleton)
     executor.set_timeout(150)
     executor.set_models(
-        [Model.GPT4O_MINI, Model.LLAMA3_1_70B, Model.QWEN2_5_32B_FP16, Model.GPT4O]
+        [Model.GPT4O_MINI, Model.LLAMA_3_1_70B_OR, Model.QWEN2_5_72B_OR, Model.GPT4O]
     )
     executor.load_instructions(
         [{"chunks": random.sample(file_chunks, 3)} for _ in range(20)]
@@ -43,11 +43,11 @@ async def run_multihop_tasks(dria: Dria, file_chunks):
 
 
 def main():
-
     # Load dataset
     dataset = load_dataset("m-ric/huggingface_doc")
-    eval_chunks = dataset["train"].select(range(int(0.10 * len(dataset["train"]))))
+    eval_chunks = dataset["train"].select(range(int(0.02 * len(dataset["train"]))))
     eval_chunks = [chunk["text"] for chunk in eval_chunks]
+    print(f"Number of evaluation chunks: {len(eval_chunks)}")
     # Create synthetic evaluation data using %10 of the dataset
     dria = Dria(rpc_token=os.environ["DRIA_RPC_TOKEN"])
     qa_eval = asyncio.run(run_qa_pipeline(dria, eval_chunks))
@@ -115,3 +115,7 @@ def main():
     )
     print(f"Incorrect: {accuracy.incorrect} ({accuracy.incorrect_percentage}%)")
     print("**********")
+
+
+if __name__ == "__main__":
+    main()
