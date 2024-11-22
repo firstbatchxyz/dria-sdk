e837f439a60a27cb1475aee79a2a6efc1cca4977
anilaltuner <altuneranilberk@gmail.com>
2024-11-22 17:47:57 +0300

    optional init for diversity group package

--- dria/utils/__init__.py
+++ dria/utils/__init__.py
@@ -1,19 +1,25 @@
+from importlib.metadata import version, PackageNotFoundError
+
 from .ec import recover_public_key, uncompressed_public_key, generate_task_keys
 from .messaging import base64_to_json, str_to_base64
 from .logging import logger
-from .metrics import NGramBasedDiversity, VendiScore
 from .formatter import FieldMapping, DataFormatter, FormatType
 
-__all__ = [
+__core_exports = [
     "recover_public_key",
     "uncompressed_public_key",
     "base64_to_json",
     "str_to_base64",
     "generate_task_keys",
     "logger",
-    "NGramBasedDiversity",
     "FieldMapping",
     "FormatType",
-    "DataFormatter",
-    "VendiScore",
+    "DataFormatter"
 ]
+
+try:
+    from .metrics import NGramBasedDiversity, VendiScore
+    version('dria[diversity]')
+    __all__ = [*__core_exports, "NGramBasedDiversity", "VendiScore"]
+except (ImportError, PackageNotFoundError):
+    __all__ = __core_exports
